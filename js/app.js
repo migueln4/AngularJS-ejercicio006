767258c71bbd14e115152eee5af254f3b3aefd3d
migueln4 <david.rodriguez@grupoonetec.com>
2018-11-20 13:38:26 +0100

    Versión vieja de AngularJS

--- js/app.js
+++ js/app.js
@@ -1,12 +1,15 @@
-(function(){
-
 var app = angular.module('universidadApp',['ngRoute']);
 
 app.controller('mainCtrl', ['$scope','$http', function($scope,$http){
 
-	$scope.menu = 'parciales/menu.html';
-  
+  $scope.menuSuperior = "parciales/menu.html";
+
+  $scope.setActive = function(opcionMenu) {
+  		$scope.menuInicio = "";
+		$scope.menuProfesores= "";
+		$scope.menuAlumnos= "";
+
+		$scope[opcionMenu] = "active";
+  }
 
 }]);
-
-})();
